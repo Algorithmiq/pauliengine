0752ba4ccc3b262db02fa5d29a97e33bd3b96e19
Ludmila Botelho <ludmila.botelho@algorithmiq.fi>
2026-02-17 15:31:59 +0000

    changed symengine version

--- tools/install-deps.sh
+++ tools/install-deps.sh
@@ -3,8 +3,8 @@
 set -euo pipefail
 
 
-curl -L https://github.com/symengine/symengine/archive/v0.14.0.tar.gz | tar -xz 
-rm -f v0.14.0.tar.gz 
-cmake -S"symengine-0.14.0" -B"symengine-0.14.0/build" -DBUILD_TESTS=OFF -DBUILD_BENCHMARKS=OFF -GNinja 
-cmake --build "symengine-0.14.0/build" --target install 
-rm -rf symengine-0.14.0 
+curl -L https://github.com/symengine/symengine/archive/master.tar.gz | tar -xz
+rm -f master.tar.gz
+cmake -S"symengine-master" -B"symengine-master/build" -DBUILD_TESTS=OFF -DBUILD_BENCHMARKS=OFF -GNinja 
+cmake --build "symengine-master/build" --target install 
+rm -rf symengine-master 
