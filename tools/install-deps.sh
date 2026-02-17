ac22db3f24bb222fc690dac74f4b194efb1f160f
Ludmila Botelho <ludmila.botelho@algorithmiq.fi>
2026-02-17 16:21:31 +0000

    trying to install symegine locally

--- tools/install-deps.sh
+++ tools/install-deps.sh
@@ -2,9 +2,16 @@
 
 set -euo pipefail
 
+PREFIX="${SYMEMENGINE_PREFIX:-$HOME/.local}"
 
 curl -L https://github.com/symengine/symengine/archive/master.tar.gz | tar -xz
 rm -f master.tar.gz
-cmake -S"symengine-master" -B"symengine-master/build" -DCMAKE_INSTALL_PREFIX=/opt/Software -DBUILD_TESTS=OFF -DBUILD_BENCHMARKS=OFF -GNinja 
-cmake --build "symengine-master/build" --target install 
-rm -rf symengine-master 
+
+cmake -S"symengine-master" -B"symengine-master/build" \
+  -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+  -DBUILD_TESTS=OFF \
+  -DBUILD_BENCHMARKS=OFF \
+  -GNinja
+
+cmake --build "symengine-master/build" --target install
+rm -rf symengine-master
